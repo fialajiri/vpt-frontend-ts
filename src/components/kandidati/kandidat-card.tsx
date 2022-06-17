924772d1e45c4c666ade76817b8d98b046053b8c
Jiri Fiala <jirik.fiala@post.cz>
2022-06-17 14:58:07 +0200

    zastupitele styling

--- src/components/kandidati/kandidat-card.tsx
+++ src/components/kandidati/kandidat-card.tsx
@@ -9,27 +9,32 @@ const KandidatCard: React.FC<KandidatCardProps> = ({ kandidat }) => {
   return (
     <div className="kandidat-card">
       <div className="kandidat-card__side kandidat-card__side--front">
-        <div className="kandidat-card__image">
-          <Image
-            src={kandidat.image}
-            alt={kandidat.name}
-            layout="fill"
-            objectFit="cover"
-          />
+        <div className="kandidat-card__image-container">
+          <div className="kandidat-card__image-box">
+            <Image
+              src={kandidat.image}
+              alt={kandidat.name}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="kandidat-card__order-container">{kandidat.order}</div>
         </div>
-        <div className="kandidat-card__order-container">{kandidat.order}</div>
       </div>
       <div className=" kandidat-card__side kandidat-card__side--back">
-        <div className="kandidat-card__order-container kandidat-card__order-container--back">
-          {kandidat.order}
-        </div>
-        <div className="kandidat-card__image--small">
-          <Image
-            src={kandidat.image}
-            alt={kandidat.name}
-            layout="fill"
-            objectFit="cover"
-          />
+        <div className="kandidat-card__image-container kandidat-card__image-container--back">          
+          <div className="kandidat-card__order-container kandidat-card__order-container--back">
+            {kandidat.order}
+          </div>
+
+          <div className="kandidat-card__image-box--small">
+            <Image
+              src={kandidat.image}
+              alt={kandidat.name}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
         <div className="kandidat-card__info">
           <p className="kandidat-card__info-name">{kandidat.name}</p>
--- src/components/zastupitele/zastupitele-list.tsx
+++ src/components/zastupitele/zastupitele-list.tsx
@@ -8,17 +8,21 @@ interface ZastupiteleProps {
 const ZastupiteleList: React.FC<ZastupiteleProps> = ({ zastupitele }) => {
   return (
     <section className="zastupitele-list__container">
-      <h1 className="headingPrimary zastupitele-list__title">Vaši Zastupitelé</h1>
-      <ul className="zastupitele-list__list">
-        {zastupitele.map((zastupitel, index) => (
-          <li key={zastupitel.id}>
-            <ZastupitelCard
-              zastupitel={zastupitel}
-              side={index % 2 === 0 ? "right" : "left"}
-            />
-          </li>
-        ))}
-      </ul>
+      <div className="zastupitele-list__wrapper">
+        <h1 className="headingPrimary zastupitele-list__title">
+          Vaši Zastupitelé
+        </h1>
+        <ul className="zastupitele-list__list">
+          {zastupitele.map((zastupitel, index) => (
+            <li key={zastupitel.id}>
+              <ZastupitelCard
+                zastupitel={zastupitel}
+                side={index % 2 === 0 ? "right" : "left"}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
