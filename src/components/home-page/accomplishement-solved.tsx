5d7615a78147686e8604eeef5251ab086a1917d9
Jiri Fiala <jirik.fiala@post.cz>
2022-06-18 22:22:28 +0200

    accoplishement update

--- src/components/home-page/accomplishement-solved.tsx
+++ src/components/home-page/accomplishement-solved.tsx
@@ -2,36 +2,35 @@ import { CheckCircle } from "phosphor-react";
 
 const AccomplishementSolved: React.FC = () => {
   return (
-    <div className='accomplishement--solved__container'>
-      <h3 className='heading-tertiary accomplishement--solved__heading'>Splněno</h3>
-      <ul className='accomplishement--solved__list'>
+    <div className="accomplishement--solved__container">
+      <h3 className="heading-tertiary accomplishement--solved__heading">Splněno</h3>
+      <ul className="accomplishement--solved__list">
         <li>
-          <CheckCircle className='accomplishement--solved__icon' />
+          <CheckCircle className="accomplishement--solved__icon" />
           <span>Omezení hazardu</span>
         </li>
         <li>
-          <CheckCircle className='accomplishement--solved__icon' />
+          <CheckCircle className="accomplishement--solved__icon" />
           <span>Komunitní plánování</span>
         </li>
         <li>
-          <CheckCircle className='accomplishement--solved__icon' />
+          <CheckCircle className="accomplishement--solved__icon" />
           <span>Započetí prací na novém územním plánu</span>
         </li>
         <li>
-          <CheckCircle className='accomplishement--solved__icon' />
+          <CheckCircle className="accomplishement--solved__icon" />
           <span>On-line přenosy z jednání zastupitelstva</span>
         </li>
         <li>
-          <CheckCircle className='accomplishement--solved__icon' />
+          <CheckCircle className="accomplishement--solved__icon" />
           <span>Pravidelná setkávání s občany</span>
         </li>
+
         <li>
-          <CheckCircle className='accomplishement--solved__icon' />
-          <span>Pravidelná setkávání s občany</span>
-        </li>
-        <li>
-          <CheckCircle className='accomplishement--solved__icon' />
-          <span>Moderní <a href="#">program</a> pro naše město</span>
+          <CheckCircle className="accomplishement--solved__icon" />
+          <span>
+            Moderní <a href="#">program</a> pro naše město
+          </span>
         </li>
       </ul>
     </div>
