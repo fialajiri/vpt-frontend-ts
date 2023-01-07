e21c700188a76024199e339c2e6a9b8e4900caf0
fialajiri <jirik.fiala@post.cz>
2023-01-07 18:59:33 +0100

    medailonek change

--- src/components/zastupitele/zastupitel-card.tsx
+++ src/components/zastupitele/zastupitel-card.tsx
@@ -10,38 +10,26 @@ interface ZastupitelProps {
 const ZastupitelCard: React.FC<ZastupitelProps> = ({ zastupitel, side }) => {
   const zastupitelImage = (
     <div className="zastupitel__card__image">
-      <Image
-        width={1000}
-        height={1000}
-        src={zastupitel.image}
-        alt={zastupitel.name}
-      />
+      <Image width={1000} height={1000} src={zastupitel.image} alt={zastupitel.name} />
     </div>
   );
 
   return (
-    <div className="zastupitel__card--container">
-      <div className="zastupitel__card--wrapper">
-        {side === "left" && zastupitelImage}
-        <blockquote className="zastupitel__card__info">
-          <p className="zastupitel__card__info--name">{zastupitel.name}</p>
+    <div className="zastupitel__card--wrapper">
+      {side === "left" && zastupitelImage}
+      <blockquote className="zastupitel__card__info">
+        <p className="zastupitel__card__info--name">{zastupitel.name}</p>
 
-          <div className="zastupitel__card__info--email-container">
-            <Envelope className="zastupitel__card__info--email-icon" />
-            <a
-              href={`mailto:${zastupitel.email}`}
-              className="zastupitel__card__info--email"
-            >
-              {zastupitel.email}
-            </a>
-          </div>
-          <p className="zastupitel__card__info--occupation">
-            {zastupitel.occupation}
-          </p>
-          <p className="zastupitel__card__info--bio">{zastupitel.bio}</p>
-        </blockquote>
-        {side === "right" && zastupitelImage}
-      </div>
+        <div className="zastupitel__card__info--email-container">
+          <Envelope className="zastupitel__card__info--email-icon" />
+          <a href={`mailto:${zastupitel.email}`} className="zastupitel__card__info--email">
+            {zastupitel.email}
+          </a>
+        </div>
+        <p className="zastupitel__card__info--occupation">{zastupitel.occupation}</p>
+        <p className="zastupitel__card__info--bio">{zastupitel.bio}</p>
+      </blockquote>
+      {side === "right" && zastupitelImage}
     </div>
   );
 };
